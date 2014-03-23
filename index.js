57010b40a8b59046ebd09fbdbb4c9eda494b1eea
Jacques DAFFLON <jacques.dafflon@usi.ch>
2014-03-23 19:57:54 +0100

    Fixed dust templates registering

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
     @description Exposes the main files
 */
 module.exports = {
-  parser    : require('./lib/parser'),
-	generator : require('./lib/markupGenerator'),
-  assessment: require('./lib/assessment')
+  assessment : require('./lib/assessment'),
+  generator  : require('./lib/markupGenerator'),
+  parser     : require('./lib/parser'),
+  templates  : require('./dusts/compiled/templates')
 }
